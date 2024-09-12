7a15ff3434668e4505e6faac2d1cf54e1bff56fc
jupe01 <juntunenreetta@gmail.com>
2024-09-12 16:39:54 +0300

    Working exercise

--- src/App.jsx
+++ src/App.jsx
@@ -4,19 +4,24 @@ import './App.css'
 function App() {
 
   const [login, setLogin] = useState(false);  
+  const [username, setUsername] = useState('');
 
   const buttonText = login ? 'Logout' : 'Login'
 
   function loginPressed(){
-    setLogin(true);
+    setLogin(!login);
   }
 
-  //something for validating email
-
-  //something if input is invalid
+  function usernameChanged(event) {
+    setUsername(event.target.value)
+  }
 
   return (
     <>
+      <h3>Tervettulloo {login ? username : ''}!!!!!</h3>
+      { !login &&
+        <input value={username} onChange={usernameChanged}/>
+      }
       <button onClick={loginPressed}>{buttonText}</button>
     </>
   )
